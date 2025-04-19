8bab7d28bc7cb8fa2349277017d818579307ce7c
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-20 00:10:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstChar(text) {
-  // your code here
+  // Trim the string to remove leading and trailing spaces
+  const trimmedText = text.trim();
+
+  // If the trimmed text is empty, return an empty string
+  if (trimmedText === '') {
+    return '';
+  }
+
+  // Return the first character of the trimmed string
+  return trimmedText[0];
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+// const text = prompt("Enter text:");
 alert(firstChar(text));
